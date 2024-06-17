a0a5bd332188df1ce9bb26153aec375b20746a5f
Andy Resha <74113797+Nestorchik@users.noreply.github.com>
2024-06-17 21:49:53 +0300

    Create download_unzip_and_delet_RUS.ps1

---
+++ download_unzip_and_delet_RUS.ps1
@@ -0,0 +1,15 @@
+cls
+write-host ----------------------------------------------------------------------------------------
+write-host Скачивание GIT...
+Invoke-WebRequest https://github.com/Nestorchik/git/releases/download/git_portable/git.zip -OutFile git.zip
+write-host Готово
+write-host ----------------------------------------------------------------------------------------
+write-host Разархивация...
+Expand-Archive -Force git.zip git_folder
+write-host Готово
+write-host ----------------------------------------------------------------------------------------
+write-host Удаление временных файлов...
+Remove-Item git.zip
+write-host Готово
+write-host ----------------------------------------------------------------------------------------
+cmd /c pause
