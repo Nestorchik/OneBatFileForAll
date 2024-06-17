2655b0a2889b97677c54aef2b72cd78caedf6d3d
Andy Resha <74113797+Nestorchik@users.noreply.github.com>
2024-06-17 20:11:56 +0300

    Create git_install_custom_nodes_no_cd.bat

---
+++ git_install_custom_nodes_no_cd.bat
@@ -0,0 +1,4 @@
+set custom_n=%CD%\ComfyUI\custom_nodes
+git clone https://github.com/Nestorchik/NStor-ComfyUI-Translation.git %custom_n%\NStor-ComfyUI-Translation
+git clone https://github.com/AIGODLIKE/AIGODLIKE-ComfyUI-Translation.git %custom_n%\AIGODLIKE-ComfyUI-Translation
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git %custom_n%\ComfyUI-Manage
