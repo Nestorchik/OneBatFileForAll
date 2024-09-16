a5ed004056f118545ee2c6bde94a810c33156077
NStor <andyr@mail.ru>
2024-09-16 21:20:41 +0300

    Update Питон_и_ComfyUI_на_русских_путях_с_пробелами.bat

--- Питон_и_ComfyUI_на_русских_путях_с_пробелами.bat
+++ Питон_и_ComfyUI_на_русских_путях_с_пробелами.bat
@@ -27,7 +27,7 @@ echo @echo off>run.bat
 echo setlocal>>run.bat
 echo cd /d "%~dp0">>run.bat
 echo set curDir="%~dp0">>run.bat
-echo set "PATH=%curDir%гит с пробелом\bin;%curDir%гит с пробелом\bin\libexec\git-core;%curDir%питон с пробелом">>run.bat
+echo set "PATH=%curDir%гит с пробелом\bin;%curDir%гит с пробелом\mingw64\bin;%curDir%гит с пробелом\mingw64\libexec\git-core;%curDir%питон с пробелом">>run.bat
 echo python "%curDir%ComfyUI\main.py" --auto-launch>>run.bat
 cd "%curDir%питон с пробелом"
 python.exe -m pip install --upgrade pip
