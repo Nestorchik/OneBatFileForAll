a010b25318add5e17ec222d6401b3a27c93108c1
NStor <andyr@mail.ru>
2024-11-02 12:56:27 +0300

    update

--- git_pull_custom_nodes_no_cd.bat
+++ git_pull_custom_nodes_no_cd.bat
@@ -1,6 +1,6 @@
 setlocal
 :: Don't forget to add the paths to the downloaded files to PATH
-:: set GIT_EXEC_PATH=%CD%\git\bin;%C D%\git\libexec;%CD%\git\libexec\git-core
+:: set GIT_EXEC_PATH=%CD%\git\bin;%CD%\git\libexec;%CD%\git\libexec\git-core
 set custom_n=%CD%\ComfyUI\custom_nodes
 git clone https://github.com/Nestorchik/NStor-ComfyUI-Translation.git %custom_n%\NStor-ComfyUI-Translation
 git clone https://github.com/AIGODLIKE/AIGODLIKE-ComfyUI-Translation.git %custom_n%\AIGODLIKE-ComfyUI-Translation
