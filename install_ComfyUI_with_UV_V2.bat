5bffbf725ff8c747738d56e25cef3c290dca8cc5
NStor <andyr@mail.ru>
2026-01-08 13:55:40 +0300

    update

--- install_ComfyUI_with_UV_V2.bat
+++ install_ComfyUI_with_UV_V2.bat
@@ -3,12 +3,12 @@ powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | ie
 uv python install 3.12
 git clone https://github.com/comfyanonymous/ComfyUI.git
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager"
-uv venv --python 3.12
+uv venv --python 3.12 --seed
 uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu129
-uv pip install pip
 uv pip install -r "%CD%\ComfyUI\requirements.txt"
+uv pip install -r "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager\requirements.txt"
 echo cd "%CD%" >run.bat
-echo uv run "%CD%\ComfyUI\main.py" --windows-standalone-build --front-end-version Comfy-Org/ComfyUI_frontend@latest >>run.bat
+echo uv run "%CD%\ComfyUI\main.py" --windows-standalone-build --preview-method latent2rgb --preview-size 512>>run.bat
 echo pause >>run.bat
-uv run "%CD%\ComfyUI\main.py" --windows-standalone-build --front-end-version Comfy-Org/ComfyUI_frontend@latest
+uv run "%CD%\ComfyUI\main.py" --windows-standalone-build
 pause
--- install_ComfyUI_with_UV_cu130.bat
+++ install_ComfyUI_with_UV_cu130.bat
@@ -1,9 +1,9 @@
-::NStor ComfyUI venv - Version from 29.09.2025
+::NStor ComfyUI venv - Version from 08.01.2026
 @echo off && setlocal && cd /D %CD%
 where uv >nul 2>&1 && uv self update || powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 git clone https://github.com/comfyanonymous/ComfyUI.git
 git clone https://github.com/ltdrdata/ComfyUI-Manager.git "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager"
-uv venv --python 3.12.11 --seed --clear
+uv venv --python 3.12.12 --seed --clear
 uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu130
 uv pip install -r "%CD%\ComfyUI\requirements.txt"
 uv pip install -r "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager\requirements.txt"
