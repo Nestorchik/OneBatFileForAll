fde34f080ce3887f8753beddae70fb88ccf3b90b
NStor <74113797+Nestorchik@users.noreply.github.com>
2026-01-08 13:29:46 +0300

    Update install_ComfyUI_with_UV_cu130.bat

--- install_ComfyUI_with_UV_cu130.bat
+++ install_ComfyUI_with_UV_cu130.bat
@@ -2,13 +2,13 @@
 @echo off && setlocal && cd /D %CD%
 where uv >nul 2>&1 && uv self update || powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 git clone https://github.com/comfyanonymous/ComfyUI.git
-git clone https://github.com/ltdrdata/ComfyUI-Manager.git "%CD%ComfyUI\custom_nodes\ComfyUI-Manager"
+git clone https://github.com/ltdrdata/ComfyUI-Manager.git "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager"
 uv venv --python 3.12.11 --seed --clear
 uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu130
-uv pip install -r "%CD%ComfyUI\requirements.txt"
-uv pip install -r "%CD%ComfyUI\custom_nodes\ComfyUI-Manager\requirements.txt"
+uv pip install -r "%CD%\ComfyUI\requirements.txt"
+uv pip install -r "%CD%\ComfyUI\custom_nodes\ComfyUI-Manager\requirements.txt"
 echo cd "%CD%" >run.bat
-echo uv run "%CD%ComfyUI\main.py" --windows-standalone-build>>run.bat
+echo uv run "%CD%\ComfyUI\main.py" --windows-standalone-build>>run.bat
 echo pause >>run.bat
-uv run "%CD%ComfyUI\main.py" --windows-standalone-build
+uv run "%CD%\ComfyUI\main.py" --windows-standalone-build
 pause
